5d889cf8f9017bc0f0a9743ff2c09393dc72871b
kristinadurant <kristinadurant.p@gmail.com>
2021-04-05 12:30:39 -0400

    added scroll to top

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
-import { Header, Home, Player } from './components'
+import ScrollToTop from './ScrollToTop';
+import { Header, Home, Player } from './components';
 
 const App = () => {
   return (
     <BrowserRouter>
+      <ScrollToTop />
       <Header />
       <Switch>
         <Route exact path='/' component={Home} />
